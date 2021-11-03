fddc2f7304f2778a0eac67442892033ac85b49e8
ShauryaSarswat <ShauryasarswatpersonalAC@gmail.com>
2021-11-03 11:40:23 +0530

    Init Codes
    Setting up the workflow and theme dictation

--- Readme.md
+++ Readme.md
@@ -1,4 +1,54 @@
 # Project : CodeX
-### Project Definition : Aplha 1
+### Project Definition : Mark 47
 
-* Module for guidance : 
+Source Code Host Link : [Github - Shaurya Sarswat](https://github.com/ShauryaSarswat/Project)
+
+## Statement :
+
+This is a modern MERN ( **Mongo , Express , React , Node** ) based **E-Commerce** website developed for easy technical blogging stuff for developers/non-developers.
+
+The wesbsites solves the problem of the Coding and technical stuff associated with owning a secure blogging website ,thus making non-developers go blogging too mainstream.
+
+- Link to the Project Abstract : []()
+
+- Link to the Project Presentation : []()
+
+- Link to the Project Demonstration : []()
+
+## Project Screenshots :
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+![This is an image](public\images\satya.jpg)
+
+## Project Tech. Analysis :
+
+- Flexiblity : Due to the **independent views** the routes can always be modified into any form thus providing the code flexiblity to be used as another *front-end* modification. Also due to the **Mongo** modern database connectivity the website *back-end* is also highly flexible to work on different schemas.
+
+- Scalability : The model of the routes that has been used with the website is *GET* ,*POST* ,*CREATE* ,*DELETE* .The routes can be extended to any level for different forms of views loading or Rendering.
+
+- Adaptability : Adaptability of the project can be extended to any level by incorporation of different technologies into the domain like *Data Science*, *Machine Learing*, *Artificial Intelligence*, *Search BOTS* . The project from the very start is planned in so very way for the incorporation of various tech. integrations and to provide more adaptable solutions.
+
+- Security : The website is secured by Node Express-Sanitizer script 
+ - > npm install express
+ - > npm install express-sanitizer
+ - > **In app.js** require("express-sanitizer")
+ 
+
+## Undersigned Introduction ( Author CV ) :
+
+
+![This is an image](public\Resume\ShauryaSarswat.png)
+
+
+## 
--- app.js
+++ app.js
@@ -1,89 +1,80 @@
-var express = require("express");//express ek parser - parser ko render - host taki views load ho jae
-//views - set template jisme tumhara print data 
-var app = express();
-var bodyParser = require("body-parser");
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/blogs', {
+var express     = require("express"),
+methodOverride  = require("method-override"),
+bodyParser      = require("body-parser"),
+expressSanitizer = require("express-sanitizer"),
+mongoose        = require("mongoose"),
+app             = express();
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-//Schema for blogs
+app.set("view engine", "ejs"); //connecting ejs
+app.use(express.static("public")); //using express
+app.use(bodyParser.urlencoded({extended: true})); //using body-parser
+app.use(methodOverride("_method"));//using method-override + what to look for in url *the parentheses as above*
+//Routes
+app.use(expressSanitizer());
 var blogSchema = new mongoose.Schema({
- title : String,
- type : String,
- coverimage : String,
- author : String,
- Description : String,
- content : String,
- date : Date
+    title: String,
+    image: String,
+    body: String,
+    created: 
+        {type: Date, default: Date.now} //i.e it should be a date and to check for default date value as of now
 });
-var blog = mongoose.model("blog", blogSchema);
-
-app.set("view engine", "ejs");
+var Blog = mongoose.model("Blog", blogSchema);
+// INDEX
 app.get("/",function(req, res){
-	
- res.render("landing");
+	res.render("landing");
 });
-app.get("/blogs",function(req, res){
-	blog.find({},function(err, Blog){
-		if(err){
-			console.log(err);
-		}
-		else{
- res.render("index",{blogs: Blog});
-		}
-	});
-});
-app.get("/blogs/createNew", function(req, res){
-	res.render("new");
-});
-app.get("/about",function(req, res){
-   res.render("about");
+app.get("/blogs", function (req, res){
+    Blog.find({}, function (err, blogs){ // adding index functionality to retrieve all blogs from database
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs}); //blogs:blogs -> render index with data (blogs is the data)
+        }
+    });
 });
 
-app.post("/blogs", function(req, res){
-   var title = req.body.title;
-	var type = req.body.type;
-	var date = req.body.date;
-	var coverimage = req.body.coverimage;
-	var author = req.body.author;
-	var Description = req.body.Description;
-	var content = req.body.content;
-	var newBlog = { 
-		title : title,
-		type : type,
-		date : date,
-		coverimage : coverimage,
-		author : author,
-		Description : Description,
-		content : content
-	};
+//CREATE ROUTE
+app.post("/blogs/new", function(req, res){
+   //create blog
+   console.log(req.body.blog.body);
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   console.log(req.body.blog.body);
+   Blog.create(req.body.blog, function (err, newBlog){
+       if(err) {
+           res.render("new");
+       } else {
+           //if successful, redirect to index
+           res.redirect("/blogs");
+       }
+   });
+});
+app.get("/blogs/new", function(req, res){
+    res.render("new");// all we have to do is render b/c its new
+});
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   })
+});
 
-	blog.create(newBlog,function(err, blog){
-		if(err){
-			console.log(err);
-		}
-		else{
-			res.redirect("/blogs");
-		}
-	});});
-   
-
-    app.get("/blogs/:id", function(req, res){
- 	blog.findById( req.params.id , function(err, blogInfo){
- 		if(err){
- 			console.log(err);
- 		}
- 		else{
- 			res.render("show", { blogs : blogInfo});
- 		}
- 	    });
-     });
-
-
-app.listen(3000,function(){
-	console.log("Your Web app started");
+app.delete("/blogs/:id", function(req, res){
+   //destroy blog
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   });
+   //redirect somewhere
+});
+app.listen(3000, function(){
+	console.log("server is running");
 })
